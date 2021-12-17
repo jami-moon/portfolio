d9d4709f8a8e17d87e05a1a1db84717c191c7287
kyukyu0312 <sherlockyu155@gmail.com>
2021-12-18 06:39:51 +0900

    ✨ Feat: Implement .skills section with responsive style

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const skillItem = document.querySelectorAll(".skills__item");
+const skillDesc = document.querySelectorAll(".skills__desc");
+
+for (let i = 0; i < skillItem.length; i++) {
+  if (i < 4) {
+    skillItem[i].addEventListener("click", () => {
+      for (let i = 0; i < 4; i++) {
+        skillItem[i].classList.remove("active");
+        skillDesc[i].classList.remove("open");
+      }
+      skillItem[i].classList.add("active");
+      skillDesc[i].classList.add("open");
+    });
+  } else {
+    skillItem[i].addEventListener("click", () => {
+      for (let i = 4; i < skillItem.length; i++) {
+        skillItem[i].classList.remove("active");
+        skillDesc[i].classList.remove("open");
+      }
+      skillItem[i].classList.add("active");
+      skillDesc[i].classList.add("open");
+    });
+  }
+}
