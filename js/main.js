dacf9b73e8aa0633bd885171100162d7380f255b
kyukyu0312 <sherlockyu155@gmail.com>
2021-12-18 18:34:59 +0900

    🐛Add: Responsive condition to the function on .skills tabmenu

--- js/main.js
+++ js/main.js
@@ -3,24 +3,28 @@
 const skillItem = document.querySelectorAll(".skills__item");
 const skillDesc = document.querySelectorAll(".skills__desc");
 
-for (let i = 0; i < skillItem.length; i++) {
-  if (i < 4) {
-    skillItem[i].addEventListener("click", () => {
-      for (let i = 0; i < 4; i++) {
-        skillItem[i].classList.remove("active");
-        skillDesc[i].classList.remove("open");
-      }
-      skillItem[i].classList.add("active");
-      skillDesc[i].classList.add("open");
-    });
-  } else {
-    skillItem[i].addEventListener("click", () => {
-      for (let i = 4; i < skillItem.length; i++) {
-        skillItem[i].classList.remove("active");
-        skillDesc[i].classList.remove("open");
-      }
-      skillItem[i].classList.add("active");
-      skillDesc[i].classList.add("open");
-    });
+// skills 섹션 탭메뉴
+// innerWidth 1200px 이하에서 열릴 때만 실행
+if (window.innerWidth < 1200) {
+  for (let i = 0; i < skillItem.length; i++) {
+    if (i < 4) {
+      skillItem[i].addEventListener("click", () => {
+        for (let i = 0; i < 4; i++) {
+          skillItem[i].classList.remove("active");
+          skillDesc[i].classList.remove("open");
+        }
+        skillItem[i].classList.add("active");
+        skillDesc[i].classList.add("open");
+      });
+    } else {
+      skillItem[i].addEventListener("click", () => {
+        for (let i = 4; i < skillItem.length; i++) {
+          skillItem[i].classList.remove("active");
+          skillDesc[i].classList.remove("open");
+        }
+        skillItem[i].classList.add("active");
+        skillDesc[i].classList.add("open");
+      });
+    }
   }
 }
